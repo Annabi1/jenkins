ae74f4787ba05f95996414221d619e39f03f244c
annabi mabrouka <66750658+Annabi1@users.noreply.github.com>
2022-12-01 08:23:18 +0100

    Update ExampleTest.java

--- src/test/java/com/dev3l/hello_world/test/ExampleTest.java
+++ src/test/java/com/dev3l/hello_world/test/ExampleTest.java
@@ -1,15 +1,43 @@
 package com.dev3l.hello_world.test;
 
-import org.junit.Assert;
+import static org.junit.Assert.assertTrue;
+ 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ExampleTest {
+	 @BeforeClass
+    public static void beforeClass() {
+        System.out.println("Before Class");
+    }
+ 
+    @Before
+    public void before() {
+        System.out.println("Before Test Case");
+    }
+ 
+    @Test
+    public void isGreaterTest() {
+        System.out.println("Test");
+        JUnitHelloWorld helloWorld = new JUnitHelloWorld();
+        assertTrue("Num 1 is greater than Num 2", helloWorld.isGreater(4, 3));
+    }
+ 
+    @After
+    public void after() {
+        System.out.println("After Test Case");
+    }
+ 
+    @AfterClass
+    public static void afterClass() {
+        System.out.println("After Class");
+    }
 	@Test
 	public void exampleTest() {
 		Assert.assertTrue(true);
 	}
-	@Test
-	public void exampleTest2() {
-		Assert.assertTrue(false);
-	}
+	
 }
